2b426a00b5828c08b29dfa78be4c42da88f03b22
mizae1dev <mizaeldevweb@gmail.com>
2026-03-01 00:10:17 -0300

    Create EnemyAI.cs script for enemy AI behavior

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,18 @@
+// EnemyAI.cs
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    void Start()
+    {
+        // Initialization code
+    }
+
+    void Update()
+    {
+        // AI logic goes here
+    }
+}
