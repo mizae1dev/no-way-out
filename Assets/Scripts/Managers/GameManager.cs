ac8d205f5a97d6de2464e3767c46ac0214ce4a67
mizae1dev <mizaeldevweb@gmail.com>
2026-03-01 00:10:14 -0300

    Create GameManager.cs file

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,16 @@
+// GameManager.cs
+
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private void Start()
+    {
+        // Initialize game states here
+    }
+
+    private void Update()
+    {
+        // Handle game logic here
+    }
+}
