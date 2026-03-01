80e400e8d47e15b4b90c5d8eb53ffd4ee8ab9bed
mizae1dev <mizaeldevweb@gmail.com>
2026-03-01 00:10:21 -0300

    Create NoiseSystem.cs

---
+++ Assets/Scripts/Systems/NoiseSystem.cs
@@ -0,0 +1,8 @@
+// NoiseSystem.cs
+// Add your noise generation logic here.
+
+using UnityEngine;
+
+public class NoiseSystem : MonoBehaviour {
+    // Your variables and methods for noise generation
+}
